14547ed94c49f85e00390915aaea81017da8f6f9
Ruijie <yinyppyin@gmail.com>
2019-04-01 16:08:48 -0400

    fixed some ui bug

--- src/containers/Favorites/FavHead/FavHead.js
+++ src/containers/Favorites/FavHead/FavHead.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import Button from "../../../UI/Button/Button";
-import SearchBar from "../../../UI/SearchBar/SearchBar";
+import Button from '../../../UI/Button/Button';
+import SearchBar from '../../../UI/SearchBar/SearchBar';
 export default function FavHead({ selected, resetSearch }) {
   return (
-    <div style={{ borderBottom: "2px solid red" }}>
+    <div style={{ borderBottom: '2px solid red', borderRadius: 0 }}>
       <SearchBar searchActivated={selected} />
       <Button
-        style={{ width: "20%", height: "40px", margin: "10px 0" }}
+        style={{ width: '20%', height: '40px', margin: '10px 0' }}
         onClick={resetSearch}
         className='btn succeed'>
         reset search
